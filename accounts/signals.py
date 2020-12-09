7444cd36be66f2f02642103ab9f66be076ba4632
KameliyaN <72250856+KameliyaN@users.noreply.github.com>
2020-12-09 09:26:15 +0200

    signals changes

--- accounts/signals.py
+++ accounts/signals.py
@@ -16,15 +16,20 @@ def create_profile(sender, instance, created, **kwargs):
         print("Profile Created!")
 
 
-def update_profile(sender, instance, created, **kwargs):
-    if not created:
-        instance.profile.first_name = instance.first_name
-        instance.profile.last_name = instance.last_name
-        instance.profile.email = instance.email
-        instance.profile.username = instance.username
-        instance.profile.save()
-        print("Profile updated!")
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
+    print("Profile saved!")
 
 
 post_save.connect(create_profile, sender=User, weak=False)
-post_save.connect(update_profile, sender=User, weak=False)
+post_save.connect(save_profile, sender=User)
+
+# def update_profile(sender, instance, created, **kwargs):
+#     if not created:
+#         instance.profile.first_name = instance.first_name
+#         instance.profile.last_name = instance.last_name
+#         instance.profile.email = instance.email
+#         instance.profile.username = instance.username
+#         instance.profile.save()
+#         print("Profile updated!")
+# post_save.connect(update_profile, sender=User, weak=False)
